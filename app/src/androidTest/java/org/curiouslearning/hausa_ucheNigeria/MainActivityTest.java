b57fab56c9607287cf51eb06267a85fc7541213f
Amit Kumar Singh <amit@sutara.org>
2026-02-10 20:53:28 +0530

    Removing Assessment's and updating manifest!

--- app/src/androidTest/java/org/curiouslearning/hausa_ucheNigeria/MainActivityTest.java
+++ app/src/androidTest/java/org/curiouslearning/hausa_ucheNigeria/MainActivityTest.java
@@ -100,7 +100,7 @@ public class MainActivityTest {
                 .check(matches(isDisplayed())).perform(click());
 
         SystemClock.sleep(3000);
-        onView(withId(R.id.settings)).perform(click());
+//        onView(withId(R.id.settings)).perform(click());
         SystemClock.sleep(1000);
         onView(withId(R.id.dropdown_menu)).perform(click());
         SystemClock.sleep(1000);
@@ -179,7 +179,7 @@ public class MainActivityTest {
     @Test
     public void test08_clickSettingButton() {
         SystemClock.sleep(1000);
-        onView(withId(R.id.settings)).perform(click());
+//        onView(withId(R.id.settings)).perform(click());
         SystemClock.sleep(1000);
         onView(withId(R.id.dropdown_menu)).perform(click());
     }
