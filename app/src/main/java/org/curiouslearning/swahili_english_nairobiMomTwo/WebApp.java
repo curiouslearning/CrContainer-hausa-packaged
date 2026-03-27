a174d3929470b4a9e7278ed0e4c1303b600a95fc
amitsinghsutara <102941445+amitsinghsutara@users.noreply.github.com>
2026-03-27 09:48:52 +0530

    Renaming Hostname

--- app/src/main/java/org/curiouslearning/swahili_english_nairobiMomTwo/WebApp.java
+++ app/src/main/java/org/curiouslearning/swahili_english_nairobiMomTwo/WebApp.java
@@ -136,7 +136,7 @@ public class WebApp extends BaseActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.addJavascriptInterface(new WebAppInterface(this), "Android");
         WebViewAssetLoader assetLoader = new WebViewAssetLoader.Builder()
-                .setDomain("nairobimomthree_swahili_english_cr-ftm-standalone.androidplatform.net")
+                .setDomain("nairobimomtwo_swahili_english_cr-ftm-standalone.androidplatform.net")
                 .addPathHandler("/assets/", new WebViewAssetLoader.AssetsPathHandler(this))
                 .build();
         webView.setWebViewClient(new WebViewClient() {
